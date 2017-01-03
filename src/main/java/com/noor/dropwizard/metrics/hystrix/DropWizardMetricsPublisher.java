c890f30c74c066ea12ea7286fe154ffcebe956f4
noorulhaq <nooremailbox@gmail.com>
2017-01-03 13:10:44 +0400

    Add files via upload

---
+++ src/main/java/com/noor/dropwizard/metrics/hystrix/DropWizardMetricsPublisher.java
@@ -0,0 +1,37 @@
+package com.noor.dropwizard.metrics.hystrix;
+
+import com.codahale.metrics.MetricRegistry;
+import com.netflix.hystrix.*;
+import com.netflix.hystrix.contrib.codahalemetricspublisher.HystrixCodaHaleMetricsPublisherThreadPool;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisherCommand;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisherThreadPool;
+
+/**
+ * Created by husainbasrawala on 1/2/17.
+ */
+public class DropWizardMetricsPublisher extends HystrixMetricsPublisher{
+
+    protected final MetricRegistry metricRegistry;
+
+    public DropWizardMetricsPublisher(MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Override
+    public HystrixMetricsPublisherCommand getMetricsPublisherForCommand(HystrixCommandKey commandKey,
+                                                                        HystrixCommandGroupKey commandGroupKey,
+                                                                        HystrixCommandMetrics metrics,
+                                                                        HystrixCircuitBreaker circuitBreaker,
+                                                                        HystrixCommandProperties properties) {
+        return new DropWizardPublisherCommand(commandKey, commandGroupKey, metrics, circuitBreaker, properties, metricRegistry);
+    }
+
+    @Override
+    public HystrixMetricsPublisherThreadPool getMetricsPublisherForThreadPool(HystrixThreadPoolKey threadPoolKey,
+                                                                              HystrixThreadPoolMetrics metrics,
+                                                                              HystrixThreadPoolProperties properties) {
+        return new HystrixCodaHaleMetricsPublisherThreadPool(threadPoolKey, metrics, properties, metricRegistry);
+    }
+
+}
---
+++ src/main/java/com/noor/dropwizard/metrics/hystrix/DropWizardPublisherCommand.java
@@ -0,0 +1,51 @@
+package com.noor.dropwizard.metrics.hystrix;
+
+/**
+ * Created by husainbasrawala on 1/2/17.
+ */
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import com.netflix.hystrix.*;
+import com.netflix.hystrix.contrib.codahalemetricspublisher.HystrixCodaHaleMetricsPublisherCommand;
+
+public class DropWizardPublisherCommand extends HystrixCodaHaleMetricsPublisherCommand {
+    protected final HystrixCommandMetrics metrics;
+    protected final MetricRegistry metricRegistry;
+
+    public DropWizardPublisherCommand(HystrixCommandKey commandKey,
+                                      HystrixCommandGroupKey commandGroupKey,
+                                      HystrixCommandMetrics metrics,
+                                      HystrixCircuitBreaker circuitBreaker,
+                                      HystrixCommandProperties properties,
+                                      MetricRegistry metricRegistry) {
+        super(commandKey, commandGroupKey, metrics, circuitBreaker, properties, metricRegistry);
+        this.metrics = metrics;
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        metricRegistry.register(createMetricName("latencyExecute_percentile_95"), new Gauge<Integer>() {
+            public Integer getValue() {
+                return metrics.getExecutionTimePercentile(95);
+            }
+        });
+        metricRegistry.register(createMetricName("latencyExecute_percentile_999"), new Gauge<Integer>() {
+            public Integer getValue() {
+                return metrics.getExecutionTimePercentile(99.9);
+            }
+        });
+        metricRegistry.register(createMetricName("latencyTotal_percentile_95"), new Gauge<Integer>() {
+            public Integer getValue() {
+                return metrics.getTotalTimePercentile(95);
+            }
+        });
+        metricRegistry.register(createMetricName("latencyTotal_percentile_999"), new Gauge<Integer>() {
+            public Integer getValue() {
+                return metrics.getTotalTimePercentile(99.9);
+            }
+        });
+    }
+}
